c4b90a50386d187abb1e1fa00f90ff4633e04098
Amritraj96 <amritraj.up.yadav@gmail.com>
2025-12-17 17:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+/* Your CSS Code here. */
+function majorityElement(arr) {
+    let count = 0;
+    let candidate = null;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (count === 0) {
+            candidate = arr[i];
+            count = 1;
+        } else if (arr[i] === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+}
